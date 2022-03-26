47388c97aaa153c192418400f684bb4d9ca3d60b
Patrick <patches72790@gmail.com>
2022-03-26 14:44:12 -0500

    setup database connection for mongodb

--- web-api-tutorial/main.go
+++ web-api-tutorial/main.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	//"os"
 	"reflect"
+	"time"
 	"tutorial/web-api/routes"
 
+	"context"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 var albums = []routes.Album{
@@ -15,8 +26,50 @@ var albums = []routes.Album{
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
+func connect() (context.Context, *mongo.Client) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	//DB_URI := fmt.Sprintf("mongodb://%s:%s@%s:%s/?maxPoolSize=20&w=majority",
+	//	os.Getenv("MONGO_DB_USERNAME"),
+	//	os.Getenv("MONGO_DB_PASS"),
+	//	os.Getenv("MONGO_HOST"),
+	//	os.Getenv("MONGO_PORT"),
+	//)
+
+	//DB_URI := "mongodb://127.0.0.1:27017"
+
+	DB_URI := fmt.Sprintf("%s", os.Getenv("MONGO_ATLAS_URI"))
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(DB_URI))
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		panic(err)
+		log.Fatal("Error pinging db client")
+	}
+
+	fmt.Println("Successfully connected and pinged client")
+
+	return ctx, client
+}
+
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	router := gin.Default()
+	connect()
 
 	albums := routes.ReadAlbumsFromDisk()
 	router.Use(routes.WriteAlbumsToDisk(albums))
@@ -57,7 +110,7 @@ func postAlbums(c *gin.Context) {
 	c.Set("album", new_albums)
 	c.IndentedJSON(http.StatusCreated, newAlbum)
 
-    c.Next()
+	c.Next()
 }
 
 func getAlbumById(c *gin.Context) {
