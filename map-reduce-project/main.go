8ff5ebc09819554b0a4d3f79a61eb5a597857064
Patrick <patches72790@gmail.com>
2022-04-02 00:22:50 -0500

    implemented mapper function for map-reduce

--- map-reduce-project/main.go
+++ map-reduce-project/main.go
@@ -4,17 +4,83 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strings"
+	"unicode"
 )
+
+type key_pair struct {
+	string
+	int
+}
+
+func sort_keys(words *map[string]int) []key_pair {
+	keys := make([]string, 0, len(*words))
+
+	for k := range *words {
+		keys = append(keys, k)
+	}
+
+	sort.Strings(keys)
+	var sorted_key_tuple []key_pair = make([]key_pair, 0, len(keys))
+
+	for _, key := range keys {
+		next_pair := struct {
+			string
+			int
+		}{key, (*words)[key]}
+
+		sorted_key_tuple = append(sorted_key_tuple, next_pair)
+	}
+
+	return sorted_key_tuple
+}
 
 func Map(contents string) {
 	word_count := map[string]int{}
 
-    tokens := strings.Split(contents, " ")
+	lines := strings.Split(contents, "\n")
 
+	var tokens []string
+	for _, line := range lines {
+		words_in_line := strings.Split(line, " ")
+		tokens = append(tokens, words_in_line...)
+	}
+
+	for _, s := range tokens {
+		var cleaned_string string
+		for _, c := range s {
+			if unicode.IsLetter(c) {
+				cleaned_string += string(c)
+			} else if !unicode.IsSpace(c) || !unicode.IsNumber(c) {
+				continue
+			}
+		}
+
+		cleaned_string = strings.ToLower(cleaned_string)
+		if value, present := word_count[cleaned_string]; present {
+			word_count[cleaned_string] = value + 1
+		} else {
+			word_count[cleaned_string] = 1
+		}
+	}
+
+	sorted_key_tuples := sort_keys(&word_count)
+
+	file, err := os.Create("frankenstein.out")
+	if err != nil {
+		panic("Error opening output file")
+	}
+
+	for _, pair := range sorted_key_tuples {
+		out_str := string(pair.string) + ":" + string(fmt.Sprint(pair.int)) + "\n"
+		file.WriteString(out_str)
+	}
 }
 
-func Reduce() {}
+func Reduce() {
+	panic("Unimplemented reduce!")
+}
 
 func main() {
 	bytes, err := os.ReadFile(os.Args[1])
@@ -24,5 +90,6 @@ func main() {
 
 	file_contents := string(bytes[:])
 
-	fmt.Println("Here are the contents of the file\n:%s", file_contents)
+	Map(file_contents)
+	Reduce()
 }
