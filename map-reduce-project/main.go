f7bf5cc2552e2920824c8eb0174fba9e90bbce7b
Patrick <patches72790@gmail.com>
2022-04-01 23:02:54 -0500

    added first map function

--- map-reduce-project/main.go
+++ map-reduce-project/main.go
@@ -1,11 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 
-func Map() {}
+func Map(contents string) {
+	word_count := map[string]int{}
+
+    tokens := strings.Split(contents, " ")
+
+}
 
 func Reduce() {}
 
 func main() {
-	fmt.Println("Hello world!")
+	bytes, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		log.Fatal("Error reading file")
+	}
+
+	file_contents := string(bytes[:])
+
+	fmt.Println("Here are the contents of the file\n:%s", file_contents)
 }
