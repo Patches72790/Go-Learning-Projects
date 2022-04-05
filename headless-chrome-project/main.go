4c3b26484b81e9ff0b07ad229718beb38c323892
Patrick <patches72790@gmail.com>
2022-04-04 19:25:51 -0500

    added screenshot for chromedb

--- headless-chrome-project/main.go
+++ headless-chrome-project/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"io/ioutil"
 	"log"
 	"os"
 	"time"
@@ -30,28 +31,38 @@ func main() {
 	password := os.Getenv("PASSWORD")
 
 	var result string
+	var buffer []byte
 	username_selector := `#clogs > input:nth-child(2)`
 	password_selector := `#clogs > input:nth-child(3)`
 	submit_selector := `#clogs-captcha-button`
-	err = chromedp.Run(ctx, submit(`https://noip.com/login`, username_selector, password_selector, submit_selector, username, password, &result))
+	err = chromedp.Run(ctx, submit(`https://noip.com/login`, username_selector,
+		password_selector, submit_selector,
+		username, password, &result, buffer))
+
+	if err := ioutil.WriteFile("screenshot.png", buffer, 0o644); err != nil {
+		log.Fatal(err)
+	}
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Printf("Go's time.After example\n%s", result)
+	log.Printf("Done with chromedp.\n")
 }
 
-func submit(urlstr, username_select, password_select, submit_selector, username, password string, result *string) chromedp.Tasks {
+func submit(urlstr, username_select,
+	password_select, submit_selector,
+	username, password string,
+	result *string, screenshot_buffer []byte) chromedp.Tasks {
 	return chromedp.Tasks{
 		chromedp.Navigate(urlstr),
 		chromedp.WaitVisible(username_select),
 		chromedp.SendKeys(username_select, username),
 		chromedp.SendKeys(password_select, password),
-		chromedp.Submit(submit_selector),
-		//chromedp.Text(`#main-navbar > div > div.navbar-header > a`, result),
-		chromedp.WaitVisible(`#content-wrapper > div:nth-child(1) > div:nth-child(1) > div > div > div > h1`, chromedp.ByID),
-		chromedp.Text(`#content-wrapper > div:nth-child(1) > div:nth-child(1) > div > div > div > h1`, result),
-		//chromedp.Text(`#dashboard-nav > a > span`, result),
+		chromedp.Click(submit_selector),
+		chromedp.WaitNotPresent(username_select),
+		chromedp.FullScreenshot(&screenshot_buffer, 90),
+		//chromedp.WaitVisible(`#content-wrapper > div:nth-child(1) > div:nth-child(1) > div > div > div > h1`),
+		//chromedp.Text(`#content-wrapper > div:nth-child(1) > div:nth-child(1) > div > div > div > h1`, result),
 	}
 }
