261c17e4f696a3e714f8f8cbbb5afeeb515af9ee
Patrick <patches72790@gmail.com>
2022-07-12 08:30:13 -0500

    added input

--- textbook-projects/gopl-ch1/main.go
+++ textbook-projects/gopl-ch1/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-    fmt.Println("hello")
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+
+    for line, n := range counts {
+        if n > 1 {
+            fmt.Println("%d\t%s\n", n, line)
+        }
+    }
 }
+
